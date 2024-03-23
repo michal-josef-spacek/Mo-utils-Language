5ed5425493d2f1e7f02e41fdae139fc72f24397b
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-03-23 15:13:52 +0100

    Add check_language_639_1() and check_language_639_2() checks

--- examples/check_language_639_1_fail.pl
+++ examples/check_language_639_1_fail.pl
@@ -4,17 +4,17 @@ use strict;
 use warnings;
 
 use Error::Pure;
-use Mo::utils::Language qw(check_language);
+use Mo::utils::Language qw(check_language_639_1);
 
 $Error::Pure::TYPE = 'Error';
 
 my $self = {
         'key' => 'xx',
 };
-check_language($self, 'key');
+check_language_639_1($self, 'key');
 
 # Print out.
 print "ok\n";
 
 # Output like:
-# #Error [...utils.pm:?] Language code 'xx' isn't ISO 639 code.
+# #Error [...utils.pm:?] Parameter 'key' doesn't contain valid ISO 639-1 code.
